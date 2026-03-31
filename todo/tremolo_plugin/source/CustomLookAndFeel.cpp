90793f3e99cfdfa6d4222db60fc65d8c4f0a2d80
HawkinWay <whq2006@whq2026deMacBook-Air.local>
2026-03-31 19:32:01 +0800

    change transparency when mouse hovers over bypassed button

--- todo/tremolo_plugin/source/CustomLookAndFeel.cpp
+++ todo/tremolo_plugin/source/CustomLookAndFeel.cpp
@@ -6,19 +6,38 @@ namespace tremolo {
         const auto bounds = button.getLocalBounds().reduced(2);
 
         if (button.getToggleState()) {
-            auto buttonGradient = juce::ColourGradient::vertical(juce::Colour{0xFFFF901A},
+            if (shouldDrawButtonAsHighlighted == true) {
+                auto buttonGradient = juce::ColourGradient::vertical(juce::Colour{0xB2FF901A},
+                                                                 juce::Colour{0xB2FFC300},bounds);
+                g.setGradientFill(buttonGradient);
+                g.fillRoundedRectangle(bounds.toFloat(),4.f);
+                g.setColour(juce::Colour{0xB250180B});
+            }
+            else {
+                auto buttonGradient = juce::ColourGradient::vertical(juce::Colour{0xFFFF901A},
                                                                  juce::Colour{0xFFFFC300},bounds);
-            g.setGradientFill(buttonGradient);
-            g.fillRoundedRectangle(bounds.toFloat(),4.f);
-            g.setColour(juce::Colour{0xFF50180B});
+                g.setGradientFill(buttonGradient);
+                g.fillRoundedRectangle(bounds.toFloat(),4.f);
+                g.setColour(juce::Colour{0xFF50180B});
+            }
         }
         else {
-            auto buttonGradient = juce::ColourGradient::vertical(juce::Colour{0xFF4A7090},
+            if (shouldDrawButtonAsHighlighted == true) {
+                auto buttonGradient = juce::ColourGradient::vertical(juce::Colour{0xB24A7090},
+                                                                 juce::Colour{0xB2324358},bounds);
+                buttonGradient.addColour(0.73,juce::Colour{0xFF315160});
+                g.setGradientFill(buttonGradient);
+                g.fillRoundedRectangle(bounds.toFloat(),4.f);
+                g.setColour(juce::Colour{0xB2DDECFF});
+            }
+            else {
+                auto buttonGradient = juce::ColourGradient::vertical(juce::Colour{0xFF4A7090},
                                                                  juce::Colour{0xFF324358},bounds);
-            buttonGradient.addColour(0.73,juce::Colour{0xFF315160});
-            g.setGradientFill(buttonGradient);
-            g.fillRoundedRectangle(bounds.toFloat(),4.f);
-            g.setColour(juce::Colour{0xFFDDECFF});
+                buttonGradient.addColour(0.73,juce::Colour{0xFF315160});
+                g.setGradientFill(buttonGradient);
+                g.fillRoundedRectangle(bounds.toFloat(),4.f);
+                g.setColour(juce::Colour{0xFFDDECFF});
+            }
         }
         g.drawText(button.getButtonText(),bounds,juce::Justification::centred, true);
     }
